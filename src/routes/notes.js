663c7b99288d4bb12062e4f4079760caf2517ca1
AngelMancilla <angelmancilla0114@gmail.com>
2023-06-28 11:15:22 -0700

    add new-note form and new routes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/notes", (req, res) => {
-  res.send("Notes is not implemented");
+router.get('/notes', (req, res) => {
+  res.send('Notes is not implemented');
+});
+
+router.get('/notes/new-note', (req, res) => {
+  res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', (req, res) => {
+  const { title, description } = req.body;
+  
+  res.send('ok');
 });
 
 module.exports = router;
